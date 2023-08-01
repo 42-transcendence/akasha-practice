e0379ac29066e84ee6350122d7f715ca7e98e94c
jkong <jkong@student.42seoul.kr>
2023-08-02 05:30:40 +0900

    애플리케이션 기반 준비
    nest
    prisma (module, service, exception filter)
    @vegardit/prisma-generator-nestjs-dto
    class-validator (interceptor)
    class-transformer (pipe)
    @nestjs/mapped-types
    @nestjs/config

---
+++ akasha-app/src/main.ts
@@ -0,0 +1,16 @@
+import { HttpAdapterHost, NestFactory, Reflector } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ClassSerializerInterceptor, ValidationPipe } from "@nestjs/common";
+import { PrismaClientExceptionFilter } from "./prisma/prisma-client-exception.filter";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const { httpAdapter } = app.get(HttpAdapterHost);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  await app.listen(3000);
+}
+bootstrap();
